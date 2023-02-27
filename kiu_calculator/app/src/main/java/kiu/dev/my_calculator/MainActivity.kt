9c9c7fe3647bc9a1f425418c9134aa9f372238fc
KiU <41310752+kyuchanE@users.noreply.github.com>
2023-02-27 16:49:40 +0900

    feat: Compose

--- kiu_calculator/app/src/main/java/kiu/dev/my_calculator/MainActivity.kt
+++ kiu_calculator/app/src/main/java/kiu/dev/my_calculator/MainActivity.kt
@@ -4,40 +4,83 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import kiu.dev.my_calculator.data.AppVersion
 import kiu.dev.my_calculator.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
-
-    override val layoutId: Int = R.layout.activity_main
-
-    private val versionData by lazy {
-        AppVersion("1.0.0")
-    }
+class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Splash init
-        installSplashScreen()
         super.onCreate(savedInstanceState)
-
-        binding.activity = this
-        binding.version = versionData.copy()
-
-        binding.btnClick.setOnClickListener {
-            Log.d("TestLog", "MainActivity btnClick setOnClickListener")
+        setContent {
+//            TextCreate()
+            Column {
+                NewStory()
+                TextCreate()
+            }
         }
-
-        setStatusBarTransparent()
-        defaultPadding(binding.clContainer)
-
     }
 
-    fun onClickEvents(v: View) {
-        when(v.id) {
-            R.id.btn_click -> {
-                Log.d("TestLog", "MainActivity btn_click onClickEvents")
+    @Preview
+    @Composable
+    private fun TextCreate() {
+        Text(text = "Hello Compose World", color = Color.White)
+    }
+
+    @Preview
+    @Composable
+    private fun NewStory() {
+        MaterialTheme {
+            val typography = MaterialTheme.typography
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(180.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "A day wandering through the sandhills " +
+                            "in Shark Fin Cove, and a few of the " +
+                            "sights I saw",
+                    color = Color.White,
+                    style = typography.bodyMedium,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "Davenport, California",
+                    color = Color.White,
+                    style = typography.bodySmall
+                )
+                Text(
+                    text = "December 2018",
+                    color = Color.White,
+                    style = typography.bodySmall
+                )
             }
         }
     }
