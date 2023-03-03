ccb217b33cb580b207ab9da6dd967b8e7dc209a5
KiU <41310752+kyuchanE@users.noreply.github.com>
2023-03-03 17:28:53 +0900

    commit

--- kiu_calculator/app/src/main/java/kiu/dev/my_calculator/MainActivity.kt
+++ kiu_calculator/app/src/main/java/kiu/dev/my_calculator/MainActivity.kt
@@ -8,14 +8,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,6 +45,7 @@ class MainActivity: AppCompatActivity() {
         Text(text = "Hello Compose World", color = Color.White)
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Preview
     @Composable
     private fun NewStory() {
@@ -81,8 +84,38 @@ class MainActivity: AppCompatActivity() {
                     color = Color.White,
                     style = typography.bodySmall
                 )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                val clicks = remember { mutableStateOf(0) }
+                Button(
+                    onClick = { ++clicks.value }
+                ) {
+                    Text(text = "Click here!")
+                }
+
+                Text(text = "Clicked ${clicks.value} times", color = Color.White)
+
+                Spacer(modifier = Modifier.height(16.dp))
+                val text = remember { mutableStateOf("Jetpack Compose") }
+
+                OutlinedTextField(
+                    value = text.value,
+                    onValueChange = { text.value = it },
+                    textStyle = TextStyle(color = Color.White)
+                )
+                TextData(input = text.value)
             }
         }
+    }
+
+    @Composable
+    private fun TextData(input: String) {
+        Log.d("@@@@@", "@@@@@ TextData input : $input")
+        val rememberedInput = remember { mutableStateOf(input) }.apply {
+            value = input
+        }
+        Text(text = " rememberedInput : ${rememberedInput.value}", color = Color.White)
     }
 
 
